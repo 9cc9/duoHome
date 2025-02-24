b829f3caf5046da08a86b1a1db8129ce46f2fd71
9cc9 <cyx199021@163.com>
2025-02-24 14:42:27 +0800

    Initial Commit

---
+++ duoHome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  duoHome
+//
+//  Created by 贝贝 on 2025/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ duoHomeTests/duoHomeTests.swift
@@ -0,0 +1,17 @@
+//
+//  duoHomeTests.swift
+//  duoHomeTests
+//
+//  Created by 贝贝 on 2025/2/24.
+//
+
+import Testing
+@testable import duoHome
+
+struct duoHomeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
